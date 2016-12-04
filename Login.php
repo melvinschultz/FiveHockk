ecc35e955d85681ff724cc58b79d937be3bc8158
melvinschultz <melvinschultzdev@gmail.com>
2016-12-04 18:59:02 +0100

    password encryption DONE

--- Login.php
+++ Login.php
@@ -12,21 +12,21 @@
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    $statement = mysqli_prepare($connect, "SELECT * FROM user WHERE email = ? AND password = ?");
-    mysqli_stmt_bind_param($statement, "ss", $email, $password);
+    $statement = mysqli_prepare($connect, "SELECT * FROM user WHERE email = ?");
+    mysqli_stmt_bind_param($statement, "s", $email);
     mysqli_stmt_execute($statement);
-
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $id, $username, $email, $password);
+    mysqli_stmt_bind_result($statement, $colId, $colUsername, $colEmail, $colPassword);
 
     $response = array();
     $response["success"] = false;
 
     while (mysqli_stmt_fetch($statement)){
-        $response["success"] = true;
-        $response["username"] = $username;
-        $response["email"] = $email;
-        $response["password"] = $password;
+        if (password_verify($password, $colPassword)) {
+            $response["success"] = true;
+            $response["username"] = $colUsername;
+            $response["email"] = $colEmail;
+        }
     }
 
     echo json_encode($response);
--- Register.php
+++ Register.php
@@ -13,17 +13,61 @@
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-//    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+    function registerUser() {
+        global $connect, $username, $email, $password;
 
-    $statement = mysqli_prepare($connect, "INSERT INTO user (username, email, password) VALUES (?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "sss", $username, $email, $password);
-    mysqli_stmt_execute($statement);
+        $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+        
+        $statement = mysqli_prepare($connect, "INSERT INTO user (username, email, password) VALUES (?, ?, ?)");
+        mysqli_stmt_bind_param($statement, "sss", $username, $email, $passwordHash);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_close($statement);
+    }
+
+    function usernameAvailable() {
+        global $connect, $username;
+
+        $statement = mysqli_prepare($connect, "SELECT * FROM user WHERE username = ?");
+        mysqli_stmt_bind_param($statement, "s", $username);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_store_result($statement);
+        $count = mysqli_stmt_num_rows($statement);
+        mysqli_stmt_close($statement);
+
+        if ($count < 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function emailAvailable() {
+        global $connect, $email;
+
+        $statement = mysqli_prepare($connect, "SELECT * FROM user WHERE email = ?");
+        mysqli_stmt_bind_param($statement, "s", $email);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_store_result($statement);
+        $count = mysqli_stmt_num_rows($statement);
+        mysqli_stmt_close($statement);
+
+        if ($count < 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     $response = array();
-    $response["username"] = $username;
-    $response["email"] = $email;
-    $response["password"] = $password;
-    $response["success"] = true;
+    //$response["username"] = $username;
+    //$response["email"] = $email;
+    //$response["password"] = $password;
+    $response["success"] = false;
+
+    if (usernameAvailable() && emailAvailable()) {
+        registerUser();
+        $response["success"] = true;
+    }
 
     echo json_encode($response);
 ?>
