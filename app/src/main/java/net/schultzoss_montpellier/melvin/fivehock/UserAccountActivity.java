e594bbdd890d632144b8e80beacc0fed98eb4ecc
melvinschultz <melvinschultzdev@gmail.com>
2016-12-18 18:35:14 +0100

    creation view and activity for question part

--- app/src/main/java/net/schultzoss_montpellier/melvin/fivehock/UserAccountActivity.java
+++ app/src/main/java/net/schultzoss_montpellier/melvin/fivehock/UserAccountActivity.java
@@ -3,6 +3,7 @@ package net.schultzoss_montpellier.melvin.fivehock;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,6 +19,17 @@ public class UserAccountActivity extends AppCompatActivity {
         final EditText editTextUsername = (EditText) findViewById(R.id.editTextUsername);
         final EditText editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         final Button buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        final TextView textViewQuestionPage = (TextView) findViewById(R.id.textViewQuestionPage);
+
+        // when a dev click on "Question Page"
+        textViewQuestionPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent questionIntent = new Intent(UserAccountActivity.this, QuestionActivity.class);
+                // start the QuestionActivity (show Question Page)
+                UserAccountActivity.this.startActivity(questionIntent);
+            }
+        });
 
         Intent intent = getIntent();
         String username = intent.getStringExtra("username");
