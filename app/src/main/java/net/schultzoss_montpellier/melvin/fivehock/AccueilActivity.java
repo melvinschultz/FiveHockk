fe367b59ee78780d413a1fb8ec8177ba15c395c7
Baptiste Domange <baptiste.domange@gmail.com>
2017-01-09 11:15:05 +0100

    Creation activity_accueil.xml, RoundedImageView.java

---
+++ app/src/main/java/net/schultzoss_montpellier/melvin/fivehock/AccueilActivity.java
@@ -0,0 +1,14 @@
+package net.schultzoss_montpellier.melvin.fivehock;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Baptiste on 09/01/2017.
+ */
+
+public class AccueilActivity extends AppCompatActivity {
+    protected void oncreate(Bundle savedInstanceState){
+
+    }
+}
