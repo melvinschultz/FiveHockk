3d24d6719dd97a1083c6a56ab751063fc3179a81
Baptiste Domange <baptiste.domange@gmail.com>
2017-01-21 23:19:11 +0100

    Vue Accueil + Progress Bar

---
+++ app/src/main/java/net/schultzoss_montpellier/melvin/fivehock/Tools/ProgressBarExample.java
@@ -0,0 +1,46 @@
+package net.schultzoss_montpellier.melvin.fivehock.Tools;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import net.schultzoss_montpellier.melvin.fivehock.R;
+
+public class ProgressBarExample extends AppCompatActivity {
+
+    ProgressBar androidProgressBar;
+    int progressStatusCounter = 0;
+    TextView textView;
+    Handler progressHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+
+        androidProgressBar = (ProgressBar) findViewById(R.id.horizontal_progress_bar);
+        textView = (TextView) findViewById(R.id.textView);
+        //Start progressing
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatusCounter < 100) {
+                    progressStatusCounter += 2;
+                    progressHandler.post(new Runnable() {
+                        public void run() {
+                            androidProgressBar.setProgress(progressStatusCounter);
+                            //Status update in textview
+                            textView.setText("Status: " + progressStatusCounter + "/" + androidProgressBar.getMax());
+                        }
+                    });
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+}
