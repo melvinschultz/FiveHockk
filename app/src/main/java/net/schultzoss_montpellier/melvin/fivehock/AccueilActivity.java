e6e6fcbe744e9431aac132863a5ed7ac6a3c6618
Baptiste Domange <baptiste.domange@gmail.com>
2017-02-06 09:27:01 +0100

    Initialisation developpement AccueilActivity

--- app/src/main/java/net/schultzoss_montpellier/melvin/fivehock/AccueilActivity.java
+++ app/src/main/java/net/schultzoss_montpellier/melvin/fivehock/AccueilActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class AccueilActivity extends AppCompatActivity {
 
@@ -14,6 +15,8 @@ public class AccueilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_accueil);
 
         final Button viewStartQuizButton = (Button) findViewById(R.id.ViewStartQuizButton);
+        final Button viewAddQuestionButton = (Button) findViewById(R.id.ViewAddQuestionButton);
+        final Button viewCorrectQuestionButton = (Button) findViewById(R.id.ViewCorrectQuestionButton);
 
         // when click on "Start a quiz"
         viewStartQuizButton.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +27,28 @@ public class AccueilActivity extends AppCompatActivity {
                 AccueilActivity.this.startActivity(questionIntent);
             }
         });
+
+        // when click on "Add a question"
+        viewAddQuestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addQuestionIntent = new Intent(AccueilActivity.this, AddQuestionActivity.class);
+                // start the QuestionActivity (show Question Page)
+                AccueilActivity.this.startActivity(addQuestionIntent);
+            }
+        });
+
+        // when click on "Correct a question"
+        viewAddQuestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            //show the user an alert
+            Toast.makeText(AccueilActivity.this, "Page non disponible pour le moment", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
     }
 }
