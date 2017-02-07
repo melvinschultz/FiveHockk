fb759d974070d030233e231832c19dbb80cae4da
Melvin Schultz <melvinschultzdev@gmail.com>
2017-02-07 16:24:53 +0100

    import bitmap drawable

--- app/src/main/java/net/schultzoss_montpellier/melvin/fivehock/UserAccountActivity.java
+++ app/src/main/java/net/schultzoss_montpellier/melvin/fivehock/UserAccountActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.provider.Settings;
